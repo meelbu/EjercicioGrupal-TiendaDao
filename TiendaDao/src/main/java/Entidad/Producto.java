46841064fb0091079fa0c45d05471cb9b297390f
Melina González <gon.melina@gmail.com>
2023-04-20 14:49:56 -0300

    Entidad

---
+++ TiendaDao/src/main/java/Entidad/Producto.java
@@ -0,0 +1,58 @@
+package Entidad;
+
+public class Producto {
+
+    private Integer codigo;
+    private String nombre;
+    private double precio;
+    private Fabricante fabricante;
+
+    public Producto(){
+
+    }
+
+    public Producto(Integer codigo, String nombre, double precio, Fabricante fabricante) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.fabricante = fabricante;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Tienda (codigo: %s, nombre: %s, precio: %s, fabricante: %s)",
+                codigo, nombre, precio, fabricante);
+    }
+}
